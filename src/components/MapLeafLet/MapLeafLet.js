0e61bd37157077e2b4055806b3bfb52f8ce1ec6b
Lucas Bracco <braccolucas1393@gmail.com>
2021-08-21 19:50:35 -0300

    ADD: map vaccination places

---
+++ src/components/MapLeafLet/MapLeafLet.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+// leaflet
+import L, { latLng } from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
+const MapLeafLet = ({place}) => {
+
+    const [position, setPosition] = useState([-29.43312678276487, -66.86011716493444]);
+
+    useEffect(() => {
+        setPosition([place.lat || -29.43312678276487, place.lon || -66.86011716493444]);
+    }, [place]);
+
+    console.log(place);
+    return (
+        <MapContainer style={{ width: '80vw', height: '80vh'}} center={position} zoom={13} scrollWheelZoom={true}>
+            <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position}>
+                <Popup>
+                    {place?.name}. <br /> {place?.address}.
+                </Popup>
+            </Marker>
+        </MapContainer>
+    );
+}
+ 
+export default MapLeafLet;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/redux/store';
+import 'leaflet/dist/leaflet.css';
 
 ReactDOM.render(
   <React.StrictMode>
